91a97e51cf8371644a370b2a9eb82aa1b4257a4e
Stronauta <samircastillop@gmail.com>
2023-10-22 19:19:54 -0400

    Se hizo la migration y se termino el code de R_Tickets

---
+++ Server/Migrations/20231022222100_Inicial.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTicketsDetalle.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    TicketsId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    SolicitadoPor = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Asunto = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.TicketsId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TicketsDetalle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TicketId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Emisor = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Mensaje = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: false),
+                    TicketsId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketsDetalle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TicketsDetalle_Tickets_TicketsId",
+                        column: x => x.TicketsId,
+                        principalTable: "Tickets",
+                        principalColumn: "TicketsId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketsDetalle_TicketsId",
+                table: "TicketsDetalle",
+                column: "TicketsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TicketsDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
