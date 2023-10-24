1274478c075806b758390bdf1b2f4147516f94a5
Stronauta <samircastillop@gmail.com>
2023-10-24 18:51:45 -0400

    Se termino el code del registro, se comenzo el code de la consulta y se resolvio un error que presentaba notificationservice

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 using RegistroTicketsDetalle.Client;
 
 namespace RegistroTicketsDetalle.Client
@@ -11,8 +12,8 @@ namespace RegistroTicketsDetalle.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddScoped<NotificationService>();
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
